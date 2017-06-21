1a803b95488b14636deb811e3490f284fbc43632
julian.escobar <julian.escobar@pragma.com.co>
2017-06-20 20:08:10 -0500

    Se crea el crud para la funcionalidad de Rol

--- mimas/src/main/java/com/mimas/rest/rolServices.java
+++ mimas/src/main/java/com/mimas/rest/rolServices.java
@@ -33,7 +33,7 @@ public class rolServices {
     @Path("/registrarRol") 
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response regisrtrarUsuario(Rol rol) throws JSONException  {
+    public Response registrarRol(Rol rol) throws JSONException  {
         JSONObject jo = new JSONObject();    
         JSONArray ja = new JSONArray();
         try {    
@@ -54,6 +54,107 @@ public class rolServices {
         } catch (Exception e) {
             jo.put("codRespuesta", "500");
             jo.put("respuesta", "Rol no registrado, error interno");
+            e.printStackTrace();
+            return Response.serverError()
+                    .entity(jo).build();
+        }
+    } 
+    
+    
+    
+    @POST
+    @Path("/actualizarRol") 
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response actualizarRol(Rol rol) throws JSONException  {
+        JSONObject jo = new JSONObject();    
+        JSONArray ja = new JSONArray();
+        try {    
+                        
+            crud = new RolCrud();
+            int respueata = (int) crud.actualizar(rol);
+            if(respueata==1){  
+               jo.put("codRespuesta", "200");
+               jo.put("respuesta", "Rol acutalizado");
+            }else{
+               jo.put("codRespuesta", "201");
+               jo.put("respuesta", "Rol no actualizado");   
+            };          
+            
+           
+            ja.put(jo);
+            return Response.status(200).entity(ja).build();
+        } catch (Exception e) {
+            jo.put("codRespuesta", "500");
+            jo.put("respuesta", "Rol no actualizado, error interno");
+            e.printStackTrace();
+            return Response.serverError()
+                    .entity(jo).build();
+        }
+    } 
+    
+    @POST
+    @Path("/consultarRol") 
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultarRol(Rol rol) throws JSONException  {
+        JSONObject jo = new JSONObject();    
+        JSONArray ja = new JSONArray();
+        try {    
+            Rol respuestaRol = new Rol();            
+            crud = new RolCrud();
+            respuestaRol = (Rol) crud.consultar(rol);
+            if(respuestaRol.getIdRol() != null){  
+               jo.put("codRespuesta", "200");
+               jo.put("respuesta", "Rol consultado");
+               jo.put("idRol", respuestaRol.getIdRol());
+               jo.put("nombre", respuestaRol.getNombre());
+               jo.put("descripcion", respuestaRol.getDescripcion());
+               jo.put("formularios", respuestaRol.getFormularios());
+               jo.put("funcionalidades", respuestaRol.getFuncionalidades());
+            }else{
+               jo.put("codRespuesta", "201");
+               jo.put("respuesta", "Rol no encontrado");   
+            };          
+            
+           
+            ja.put(jo);
+            return Response.status(200).entity(ja).build();
+        } catch (Exception e) {
+            jo.put("codRespuesta", "500");
+            jo.put("respuesta", "Rol no registrado, error interno");
+            e.printStackTrace();
+            return Response.serverError()
+                    .entity(jo).build();
+        }
+    }
+    
+    
+    @POST
+    @Path("/eliminarRol") 
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response eliminarRol(Rol rol) throws JSONException  {
+        JSONObject jo = new JSONObject();    
+        JSONArray ja = new JSONArray();
+        try {    
+                        
+            crud = new RolCrud();
+            int respueata = (int) crud.eliminar(rol);
+            if(respueata==1){  
+               jo.put("codRespuesta", "200");
+               jo.put("respuesta", "Rol eliminado");
+            }else{
+               jo.put("codRespuesta", "201");
+               jo.put("respuesta", "Rol no eliminado");   
+            };          
+            
+           
+            ja.put(jo);
+            return Response.status(200).entity(ja).build();
+        } catch (Exception e) {
+            jo.put("codRespuesta", "500");
+            jo.put("respuesta", "Rol no eliminado, error interno");
             e.printStackTrace();
             return Response.serverError()
                     .entity(jo).build();
