f358dd92b71fb9c4e3391ee26b85c453c2657689
Mario Andrés Ruiz <marioarb.97@gmail.com>
2017-06-24 04:06:13 -0500

    sql para crear tabla de fundaciones

--- mimas/src/main/java/com/mimas/util/sql.sql
+++ mimas/src/main/java/com/mimas/util/sql.sql
@@ -1,12 +1,13 @@
 create table fundaciones (
 	identificacion varchar(25),
 	razonSocial varchar(75) NOT NULL,
-	telefonoFijo varchar(15) NOT NULL,
-	telefonoMovil varchar(15) NOT NULL,
+	telefonoFijo varchar(15) NULL,
+	telefonoMovil varchar(15) NULL,
 	email varchar(50) NOT NULL,
 	direccion varchar (75) NOT NULL,
 	usuario varchar (25) NOT NULL,
-	tipoEntidad varchar(50) NOT NULL
+	tipoEntidad varchar(50) NOT NULL,
+	
+	constraint PK_IDENTIFICACION_FUNDACIONES primary key (identificacion)
 
-)
-add constraint PK_IDENTIFICACION_FUNDACIONES primary key (identificacion);
+);
