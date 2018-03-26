8fa5af4295c9e2455fdc8d4ece7c61fb14d42593
attil <attilaescobar@hotmail.com>
2018-03-25 20:18:39 -0500

    Avanzo en funcionalidad para consultar y actualizar solicitud de adopción.

--- src/main/java/com/mimas/crud/AdopcionCrud.java
+++ src/main/java/com/mimas/crud/AdopcionCrud.java
@@ -2,10 +2,12 @@ package com.mimas.crud;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Date;
 
 import com.mimas.model.Adopcion;
+import com.mimas.model.Mascota;
 import com.mimas.util.conexionDB;
 
 public class AdopcionCrud implements CrudInterface {
@@ -27,7 +29,7 @@ public class AdopcionCrud implements CrudInterface {
 		preparedStatement.setDate(3, new java.sql.Date(0L));
 		preparedStatement.setString(4, adopcion.getNombreAdjunto());
 		preparedStatement.setString(5, adopcion.getAdjunto());
-		preparedStatement.setString(6, "1");
+		preparedStatement.setString(6, "En proceso");
         respuesta = preparedStatement.executeUpdate();  
         preparedStatement.close();
         con.close();
@@ -36,12 +38,46 @@ public class AdopcionCrud implements CrudInterface {
 
 	@Override
 	public Object consultar(Object consultar) throws Exception {
-		return null;
+        ResultSet respuesta;
+        Adopcion adopcion = (Adopcion) consultar;         
+        con = conexionDB.getConexion(); 
+        PreparedStatement preparedStatement = null ;
+        String sql = "Select   usuario, mascota,fecha, nombreadjunto, adjunto, estado from adopcion where idSolicitud = ?";       
+        preparedStatement  = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, adopcion.getIdAdopcion());        
+        respuesta = preparedStatement.executeQuery();
+        Adopcion adopcionRespuesta = new Adopcion(); 
+        while(respuesta.next()){
+        	adopcionRespuesta.setUsuario(respuesta.getString("usuario"));
+        	adopcionRespuesta.setIdMascota(respuesta.getString("mascota"));
+        	adopcionRespuesta.setFecha(respuesta.getDate("fecha"));
+        	adopcionRespuesta.setNombreAdjunto(respuesta.getString("nombreadjunto"));
+        	adopcionRespuesta.setAdjunto(respuesta.getString("adjunto"));
+        	adopcionRespuesta.setEstadoSolicitud(respuesta.getString("estado"));
+        }
+        preparedStatement.close();
+        con.close();        
+        return adopcionRespuesta;
 	}
-
+	
 	@Override
-	public Object actualizar(Object consultar) throws Exception {
-		return null;
+	public Object actualizar(Object actualizar) throws Exception {
+		int respuesta = 0;
+		Adopcion adopcion = (Adopcion) actualizar;        
+        con = conexionDB.getConexion(); 
+        PreparedStatement preparedStatement = null ;
+        String sql = "update adopcion set  mascota = ?, fecha = ? , nombreadjunto = ?, adjunto = ?, estado = ? where idSolicitud = ?";             
+		preparedStatement  = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		preparedStatement.setString(1, adopcion.getIdMascota());
+		preparedStatement.setDate(2, new java.sql.Date(0L));
+		preparedStatement.setString(3, adopcion.getNombreAdjunto());
+		preparedStatement.setString(4, adopcion.getAdjunto());
+		preparedStatement.setString(5, adopcion.getEstadoSolicitud());
+		preparedStatement.setString(6, adopcion.getIdAdopcion());
+        respuesta = preparedStatement.executeUpdate();  
+        preparedStatement.close();
+        con.close();        
+        return respuesta;
 	}
 
 	@Override
--- src/main/java/com/mimas/rest/AdopcionServices.java
+++ src/main/java/com/mimas/rest/AdopcionServices.java
@@ -6,8 +6,11 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -78,5 +81,69 @@ public class AdopcionServices {
         }     
 
    }
+    
+    @GET
+    @Path("/adopcion") 
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultarMascota(@QueryParam("id") String id) throws JSONException  {
+        JSONObject jo = new JSONObject();    
+        JSONArray ja = new JSONArray();
+        try {  
+        	Adopcion adopcion = new Adopcion();
+        	adopcion.setIdAdopcion(id);
+            Adopcion respuestaAdopcion = new Adopcion();            
+            crud = new AdopcionCrud();
+            respuestaAdopcion = (Adopcion) crud.consultar(adopcion);
+            if(respuestaAdopcion.getIdMascota() != null){  
+               jo.put("codRespuesta", "200");
+               jo.put("respuesta", "Solicitud consultada");              
+               jo.put("usuario", respuestaAdopcion.getUsuario());
+               jo.put("idMascota", respuestaAdopcion.getIdMascota());
+               jo.put("nombreAdjunto", respuestaAdopcion.getNombreAdjunto());
+               jo.put("adjunto", respuestaAdopcion.getAdjunto());
+               jo.put("estado", respuestaAdopcion.getEstadoSolicitud());
+            }else{
+               jo.put("codRespuesta", "201");
+               jo.put("respuesta", "Solicitud no encontrada");   
+            };               
+            ja.put(jo);
+            return Response.status(200).entity(ja).build();
+        } catch (Exception e) {
+            jo.put("codRespuesta", "500");
+            jo.put("respuesta", "Solicitud no encontrada, error interno");
+            e.printStackTrace();
+            return Response.serverError()
+                    .entity(jo).build();
+        }
+    } 
+    
+    @PUT
+    @Path("/adopcion") 
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response actualizarRol(Adopcion adopcion) throws JSONException  {
+        JSONObject jo = new JSONObject();    
+        JSONArray ja = new JSONArray();
+        try {    
+                        
+            crud = new AdopcionCrud();
+            int respueata = (int) crud.actualizar(adopcion);
+            if(respueata==1){  
+               jo.put("codRespuesta", "200");
+               jo.put("respuesta", "Solicitud actualizadoa");
+            }else{
+               jo.put("codRespuesta", "201");
+               jo.put("respuesta", "Solicitud no actualizada");   
+            };          
+            ja.put(jo);
+            return Response.status(200).entity(ja).build();
+        } catch (Exception e) {
+            jo.put("codRespuesta", "500");
+            jo.put("respuesta", "Solicitud no actualizada, error interno");
+            e.printStackTrace();
+            return Response.serverError()
+                    .entity(jo).build();
+        }
+    }
 
 }
