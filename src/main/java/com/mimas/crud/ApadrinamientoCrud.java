3a68fab5ce9162063438371e4125bf55b38ad0b2
Laura <Laura>
2018-04-24 10:42:09 -0500

    Actualización Apadrinamiento listar

--- src/main/java/com/mimas/crud/ApadrinamientoCrud.java
+++ src/main/java/com/mimas/crud/ApadrinamientoCrud.java
@@ -2,10 +2,13 @@ package com.mimas.crud;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.mimas.model.Adopcion;
 import com.mimas.model.Apadrinamiento;
 import com.mimas.util.conexionDB;
 
@@ -37,12 +40,46 @@ public class ApadrinamientoCrud implements CrudInterface {
 
 	@Override
 	public Object consultar(Object consultar) throws Exception {
-		return null;
+        ResultSet respuesta;
+        Apadrinamiento apadrinamiento = (Apadrinamiento) consultar;         
+        con = conexionDB.getConexion(); 
+        PreparedStatement preparedStatement = null ;
+        String sql = "Select   usuario, mascota,fecha, nombreadjunto, adjunto, estado from apadrinamiento where IdApadrinamiento = ?";       
+        preparedStatement  = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, apadrinamiento.getIdApadrinamiento());        
+        respuesta = preparedStatement.executeQuery();
+        Apadrinamiento apadrinamientoRespuesta = new Apadrinamiento(); 
+        while(respuesta.next()){
+        	apadrinamientoRespuesta.setUsuario(respuesta.getString("usuario"));
+        	apadrinamientoRespuesta.setIdMascota(respuesta.getString("mascota"));
+        	apadrinamientoRespuesta.setFecha(respuesta.getDate("fecha"));
+        	apadrinamientoRespuesta.setNombreAdjunto(respuesta.getString("nombreadjunto"));
+        	apadrinamientoRespuesta.setAdjunto(respuesta.getString("adjunto"));
+        	apadrinamientoRespuesta.setEstadoSolicitud(respuesta.getString("estado"));
+        }
+        preparedStatement.close();
+        con.close();        
+        return apadrinamientoRespuesta;
 	}
-
+	
 	@Override
-	public Object actualizar(Object consultar) throws Exception {
-		return null;
+	public Object actualizar(Object actualizar) throws Exception {
+		int respuesta = 0;
+		Apadrinamiento apadrinamiento = (Apadrinamiento) actualizar;        
+        con = conexionDB.getConexion(); 
+        PreparedStatement preparedStatement = null ;
+        String sql = "update apadrinamiento set  mascota = ?, fecha = ? , nombreadjunto = ?, adjunto = ?, estado = ? where IdApadrinamiento = ?";             
+		preparedStatement  = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		preparedStatement.setString(1, apadrinamiento.getIdMascota());
+		preparedStatement.setDate(2, new java.sql.Date(0L));
+		preparedStatement.setString(3, apadrinamiento.getNombreAdjunto());
+		preparedStatement.setString(4, apadrinamiento.getAdjunto());
+		preparedStatement.setString(5, apadrinamiento.getEstadoSolicitud());
+		preparedStatement.setString(6, apadrinamiento.getIdApadrinamiento());
+        respuesta = preparedStatement.executeUpdate();  
+        preparedStatement.close();
+        con.close();        
+        return respuesta;
 	}
 
 	@Override
@@ -52,8 +89,28 @@ public class ApadrinamientoCrud implements CrudInterface {
 
 	@Override
 	public List<Object> listar() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+        ResultSet respuesta;
+        con = conexionDB.getConexion(); 
+        PreparedStatement preparedStatement = null ;
+        String sql = "Select IdApadrinamiento,  usuario, mascota,fecha, nombreadjunto, adjunto, estado from apadrinamiento";       
+        preparedStatement  = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        respuesta = preparedStatement.executeQuery();
+        List<Object> listApadrinamiento = new ArrayList<Object>();
+        Apadrinamiento miapadrinamiento = null;
+        while(respuesta.next()){
+        	miapadrinamiento = new Apadrinamiento();
+        	miapadrinamiento.setIdApadrinamiento(respuesta.getString("IdApadrinamiento"));
+        	miapadrinamiento.setUsuario(respuesta.getString("usuario"));
+        	miapadrinamiento.setIdMascota(respuesta.getString("mascota"));
+        	miapadrinamiento.setFecha(respuesta.getDate("fecha"));
+        	miapadrinamiento.setNombreAdjunto(respuesta.getString("nombreadjunto"));
+        	miapadrinamiento.setAdjunto(respuesta.getString("adjunto"));
+        	miapadrinamiento.setEstadoSolicitud(respuesta.getString("estado"));
+        	listApadrinamiento.add(miapadrinamiento);
+        }
+        preparedStatement.close();
+        con.close();        
+        return listApadrinamiento;
 	}
 
 }
--- src/main/java/com/mimas/rest/ApadrinamientoServices.java
+++ src/main/java/com/mimas/rest/ApadrinamientoServices.java
@@ -1,13 +1,17 @@
 package com.mimas.rest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -16,12 +20,10 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 import com.google.gson.Gson;
-import com.mimas.crud.AdopcionCrud;
 import com.mimas.crud.ApadrinamientoCrud;
 import com.mimas.crud.CrudInterface;
 import com.mimas.crud.FundacionCrud;
 import com.mimas.crud.MascotaCrud;
-import com.mimas.model.Adopcion;
 import com.mimas.model.Apadrinamiento;
 import com.mimas.model.Mascota;
 
@@ -73,12 +75,100 @@ public class ApadrinamientoServices {
             return Response.status(200).entity(ja).build();
         } catch (Exception e) {
             jo.put("codRespuesta", "500");
-            jo.put("respuesta", "Solicitud no registrada, error");
+            jo.put("respuesta", "Solicitud no registrada¡, error interno");
             e.printStackTrace();
              return Response.serverError()
                     .entity(jo).build();
         }     
 
    }
+    
+    @GET
+    @Path("/list-apadrinamiento") 
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listarSolicitud() throws JSONException  {
+        JSONObject jo = new JSONObject();    
+        JSONArray ja = new JSONArray();
+        try {  
+            crud = new ApadrinamientoCrud();
+            List<Apadrinamiento> listaSolicitud = new ArrayList<>();
+            listaSolicitud = (List<Apadrinamiento>)(List<?>) crud.listar();
+//            jo.put("codRespuesta", "200");
+//            jo.put("listaSolicitud", listaSolicitud);                             
+//            ja.put(jo);
+            return Response.status(200).entity(listaSolicitud).build();
+        } catch (Exception e) {
+            jo.put("codRespuesta", "500");
+            jo.put("respuesta", "Solicitud no encontrada, error interno");
+            e.printStackTrace();
+            return Response.serverError()
+                    .entity(jo).build();
+            
+        }
+    } 
+    
+    @GET
+    @Path("/apadrinamiento") 
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultarSolicitud(@QueryParam("id") String id) throws JSONException  {
+        JSONObject jo = new JSONObject();    
+        JSONArray ja = new JSONArray();
+        try {  
+        	Apadrinamiento apadrinamiento = new Apadrinamiento();
+        	apadrinamiento.setIdApadrinamiento(id);
+        	Apadrinamiento respuestaApadrinamiento = new Apadrinamiento();            
+            crud = new ApadrinamientoCrud();
+            respuestaApadrinamiento = (Apadrinamiento) crud.consultar(apadrinamiento);
+            if(respuestaApadrinamiento.getIdMascota() != null){  
+               jo.put("codRespuesta", "200");
+               jo.put("respuesta", "Solicitud consultada");              
+               jo.put("usuario", respuestaApadrinamiento.getUsuario());
+               jo.put("idMascota", respuestaApadrinamiento.getIdMascota());
+               jo.put("nombreAdjunto", respuestaApadrinamiento.getNombreAdjunto());
+               jo.put("adjunto", respuestaApadrinamiento.getAdjunto());
+               jo.put("estado", respuestaApadrinamiento.getEstadoSolicitud());
+            }else{
+               jo.put("codRespuesta", "201");
+               jo.put("respuesta", "Solicitud no encontrada");   
+            };               
+            ja.put(jo);
+            return Response.status(200).entity(ja).build();
+        } catch (Exception e) {
+            jo.put("codRespuesta", "500");
+            jo.put("respuesta", "Solicitud no encontrada, error interno");
+            e.printStackTrace();
+            return Response.serverError()
+                    .entity(jo).build();
+        }
+    } 
+    
+    @PUT
+    @Path("/apadrinamiento") 
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response actualizarRol(Apadrinamiento apadrinamiento) throws JSONException  {
+        JSONObject jo = new JSONObject();    
+        JSONArray ja = new JSONArray();
+        try {    
+                        
+            crud = new ApadrinamientoCrud();
+            int respueata = (int) crud.actualizar(apadrinamiento);
+            if(respueata==1){  
+               jo.put("codRespuesta", "200");
+               jo.put("respuesta", "Solicitud actualizadoa");
+            }else{
+               jo.put("codRespuesta", "201");
+               jo.put("respuesta", "Solicitud no actualizada");   
+            };          
+            ja.put(jo);
+            return Response.status(200).entity(ja).build();
+        } catch (Exception e) {
+            jo.put("codRespuesta", "500");
+            jo.put("respuesta", "Solicitud no actualizada, error interno");
+            e.printStackTrace();
+            return Response.serverError()
+                    .entity(jo).build();
+        }
+    }
 
 }
